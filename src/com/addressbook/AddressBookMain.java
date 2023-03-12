4cccfc2b4e73176cd89c83ba275cf470c0adb0f1
kundan kumar <kundanchauhan1997@gmail.com>
2023-03-12 21:26:23 +0530

    Added: Ablity to edit details in address book

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -5,33 +5,84 @@ import java.util.Scanner;
 
 public class AddressBookMain extends ContactPerson {
 
+    public static AddressBookMain newPersonDetails = new AddressBookMain();
+    static ArrayList<ContactPerson> personDetails = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+
+    public void menu() {
+        int menu;
+        System.out.println("Enter 1 to add contact:");
+        System.out.println("Enter 2 to edit contact:");
+        System.out.println("Enter 0 to exit:");
+        menu = scanner.nextInt();
+        while (menu != 0) {
+            switch (menu) {
+                case 1 -> newPersonDetails.addContact();
+                case 2 -> newPersonDetails.editDetails();
+            }
+            System.out.println("Enter 1 to add contact:");
+            System.out.println("Enter 2 to edit contact:");
+            System.out.println("Enter 0 to exit:");
+            menu = scanner.nextInt();
+        }
+        System.out.println("Goodbye from addressBook");
+    }
+
     public void addContact() {
-        ArrayList<AddressBookMain> personDetails = new ArrayList<>();
-        AddressBookMain newpersonDetails = new AddressBookMain();
+
         // Taking user input
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter first name: ");
-        newpersonDetails.setFirstName(scanner.nextLine());
+        newPersonDetails.setFirstName(scanner.next());
         System.out.println("Enter last name: ");
-        newpersonDetails.setLastName(scanner.nextLine());
+        newPersonDetails.setLastName(scanner.next());
         System.out.println("Enter Address: ");
-        newpersonDetails.setAddress(scanner.nextLine());
+        newPersonDetails.setAddress(scanner.next());
         System.out.println("Enter city: ");
-        newpersonDetails.setCity(scanner.nextLine());
+        newPersonDetails.setCity(scanner.next());
         System.out.println("Enter state: ");
-        newpersonDetails.setCity(scanner.nextLine());
+        newPersonDetails.setCity(scanner.next());
         System.out.println("Enter zip: ");
-        newpersonDetails.setZip(scanner.nextInt());
+        newPersonDetails.setZip(scanner.nextInt());
         System.out.println("Enter phone_number: ");
-        newpersonDetails.setPhoneNumber(scanner.nextLong());
+        newPersonDetails.setPhoneNumber(scanner.nextLong());
         System.out.println("Enter email: ");
-        newpersonDetails.setEmail(scanner.next());
+        newPersonDetails.setEmail(scanner.next());
         // Adding details in address book
-        personDetails.add(newpersonDetails);
+        personDetails.add(newPersonDetails);
         System.out.println(personDetails);
     }
+
+    // Edit the contact with name
+    public void editDetails() {
+        System.out.println("Enter first name you want to edit:");
+        String choiceName = scanner.next();
+        // AddressBookMain newPersonDetails = new AddressBookMain();
+        if (choiceName.equals(newPersonDetails.getFirstName())) {
+            System.out.println("Edit first name:");
+            newPersonDetails.setFirstName(scanner.next());
+            System.out.println("Edit last name:");
+            newPersonDetails.setLastName(scanner.next());
+            System.out.println("Edit address:");
+            newPersonDetails.setAddress(scanner.next());
+            System.out.println("Edit city:");
+            newPersonDetails.setCity(scanner.next());
+            System.out.println("Edit state:");
+            newPersonDetails.setState(scanner.next());
+            System.out.println("Edit zip:");
+            newPersonDetails.setZip(scanner.nextInt());
+            System.out.println("Edit Phone_Number:");
+            newPersonDetails.setPhoneNumber(scanner.nextLong());
+            System.out.println("Edit gmail:");
+            newPersonDetails.setEmail(scanner.next());
+
+            //  personDetails.add(newPersonDetails);
+            System.out.println(personDetails);
+        } else {
+            System.out.println("Please Enter A Valid Name:");
+        }
+    }
+
     public static void main(String[] args) {
-        AddressBookMain addressBookMain = new AddressBookMain();
-        addressBookMain.addContact();
+        newPersonDetails.menu();
     }
 }
