6c80a4990733b94e0c89d69cde806ae44d3d0e8a
kundan kumar <kundanchauhan1997@gmail.com>
2023-03-12 08:58:36 +0530

    Added: Welcome message

---
+++ src/com/addressbook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.addressbook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To AddressBook Management System");
+    }
+}
